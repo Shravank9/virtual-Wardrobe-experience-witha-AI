8e4c8c29dd96ebe8411e50739204027252c9e3b3
Gandesiri Shravan Kumar <162951704+Shravank9@users.noreply.github.com>
2026-05-17 22:39:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,266 @@
-import os, sys
+import subprocess
+import sys
+
+# Install OpenCV if missing
+subprocess.check_call([
+    sys.executable,
+    "-m",
+    "pip",
+    "install",
+    "opencv-python-headless"
+])
+
 import cv2
+import os
 from PIL import Image
 import numpy as np
 import glob
 import warnings
 import argparse
+
 from cloths_segmentation.pre_trained_models import create_model
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--background', type=bool, default=True, help='Define removing background or not')
+
+    parser.add_argument(
+        '--background',
+        type=bool,
+        default=True,
+        help='Define removing background or not'
+    )
+
     opt = parser.parse_args()
-    
-    # Read input image
-    img=cv2.imread("./static/origin_web.jpg")
-    ori_img=cv2.resize(img,(768,1024))
-    cv2.imwrite("./origin.jpg",ori_img)
 
-    # Resize input image
-    img=cv2.imread('origin.jpg')
-    img=cv2.resize(img,(384,512))
-    cv2.imwrite('resized_img.jpg',img)
+    # =====================================
+    # READ INPUT IMAGE
+    # =====================================
 
-    # Get mask of cloth
-    print("Get mask of cloth\n")
-    terminnal_command = "python3 get_cloth_mask.py" 
-    os.system(terminnal_command)
+    img = cv2.imread("./static/origin_web.jpg")
 
-    # Get openpose coordinate using posenet
-    print("Get openpose coordinate using posenet\n")
-    terminnal_command = "python3 posenet.py" 
-    os.system(terminnal_command)
+    if img is None:
 
-    # Generate semantic segmentation using Graphonomy-Master library
-    print("Generate semantic segmentation using Graphonomy-Master library\n")
+        print("Input image not found")
+        sys.exit()
+
+    ori_img = cv2.resize(
+        img,
+        (768, 1024)
+    )
+
+    cv2.imwrite(
+        "./origin.jpg",
+        ori_img
+    )
+
+    # =====================================
+    # RESIZE IMAGE
+    # =====================================
+
+    img = cv2.imread('origin.jpg')
+
+    img = cv2.resize(
+        img,
+        (384, 512)
+    )
+
+    cv2.imwrite(
+        'resized_img.jpg',
+        img
+    )
+
+    # =====================================
+    # GET CLOTH MASK
+    # =====================================
+
+    print("Get mask of cloth")
+
+    os.system(
+        "python get_cloth_mask.py"
+    )
+
+    # =====================================
+    # GET POSENET
+    # =====================================
+
+    print("Get openpose coordinate using posenet")
+
+    os.system(
+        "python posenet.py"
+    )
+
+    # =====================================
+    # GRAPHONOMY SEGMENTATION
+    # =====================================
+
+    print("Generate semantic segmentation")
+
     os.chdir("./Graphonomy-master")
-    terminnal_command ="python3 exp/inference/inference.py --loadmodel ./inference.pth --img_path ../resized_img.jpg --output_path ../ --output_name /resized_segmentation_img"
-    os.system(terminnal_command)
+
+    os.system(
+        "python exp/inference/inference.py "
+        "--loadmodel ./inference.pth "
+        "--img_path ../resized_img.jpg "
+        "--output_path ../ "
+        "--output_name /resized_segmentation_img"
+    )
+
     os.chdir("../")
 
-    # Remove background image using semantic segmentation mask
-    mask_img=cv2.imread('./resized_segmentation_img.png',cv2.IMREAD_GRAYSCALE)
-    mask_img=cv2.resize(mask_img,(768,1024))
-    k = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-    mask_img = cv2.erode(mask_img, k)
-    img_seg=cv2.bitwise_and(ori_img,ori_img,mask=mask_img)
-    back_ground=ori_img-img_seg
-    img_seg=np.where(img_seg==0,215,img_seg)
-    cv2.imwrite("./seg_img.png",img_seg)
-    img=cv2.resize(img_seg,(768,1024))
-    cv2.imwrite('./HR-VITON-main/test/test/image/00001_00.jpg',img)
+    # =====================================
+    # LOAD SEGMENTATION MASK
+    # =====================================
 
-    # Generate grayscale semantic segmentation image
-    terminnal_command ="python3 get_seg_grayscale.py"
-    os.system(terminnal_command)
+    mask_img = cv2.imread(
+        './resized_segmentation_img.png',
+        cv2.IMREAD_GRAYSCALE
+    )
 
-    # Generate Densepose image using detectron2 library
-    print("\nGenerate Densepose image using detectron2 library\n")
-    terminnal_command ="python3 detectron2/projects/DensePose/apply_net.py dump detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml \
-    https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl \
-    origin.jpg --output output.pkl -v"
-    os.system(terminnal_command)
-    terminnal_command ="python3 get_densepose.py"
-    os.system(terminnal_command)
+    if mask_img is None:
 
-    # Run HR-VITON to generate final image
-    print("\nRun HR-VITON to generate final image\n")
+        print("Segmentation image not generated")
+        sys.exit()
+
+    mask_img = cv2.resize(
+        mask_img,
+        (768, 1024)
+    )
+
+    k = cv2.getStructuringElement(
+        cv2.MORPH_RECT,
+        (3, 3)
+    )
+
+    mask_img = cv2.erode(
+        mask_img,
+        k
+    )
+
+    img_seg = cv2.bitwise_and(
+        ori_img,
+        ori_img,
+        mask=mask_img
+    )
+
+    back_ground = ori_img - img_seg
+
+    img_seg = np.where(
+        img_seg == 0,
+        215,
+        img_seg
+    )
+
+    cv2.imwrite(
+        "./seg_img.png",
+        img_seg
+    )
+
+    img = cv2.resize(
+        img_seg,
+        (768, 1024)
+    )
+
+    cv2.imwrite(
+        './HR-VITON-main/test/test/image/00001_00.jpg',
+        img
+    )
+
+    # =====================================
+    # GENERATE GRAYSCALE SEGMENTATION
+    # =====================================
+
+    os.system(
+        "python get_seg_grayscale.py"
+    )
+
+    # =====================================
+    # DENSEPOSE
+    # =====================================
+
+    print("Generate Densepose image")
+
+    os.system(
+        "python detectron2/projects/DensePose/apply_net.py "
+        "dump "
+        "detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_s1x.yaml "
+        "https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_s1x/165712039/model_final_162be9.pkl "
+        "origin.jpg "
+        "--output output.pkl -v"
+    )
+
+    os.system(
+        "python get_densepose.py"
+    )
+
+    # =====================================
+    # RUN HR-VITON
+    # =====================================
+
+    print("Run HR-VITON")
+
     os.chdir("./HR-VITON-main")
-    terminnal_command = "python3 test_generator.py --cuda True --test_name test1 --tocg_checkpoint mtviton.pth --gpu_ids 0 --gen_checkpoint gen.pth --datasetting unpaired --data_list t2.txt --dataroot ./test" 
-    os.system(terminnal_command)
 
-    # Add Background or Not
-    l=glob.glob("./Output/*.png")
+    os.system(
+        "python test_generator.py "
+        "--cuda False "
+        "--test_name test1 "
+        "--tocg_checkpoint mtviton.pth "
+        "--gpu_ids -1 "
+        "--gen_checkpoint gen.pth "
+        "--datasetting unpaired "
+        "--data_list t2.txt "
+        "--dataroot ./test"
+    )
 
-    # Add Background
+    # =====================================
+    # OUTPUT IMAGES
+    # =====================================
+
+    l = glob.glob("./Output/*.png")
+
+    if len(l) == 0:
+
+        print("No output images generated")
+
+        sys.exit()
+
+    # ADD BACKGROUND
     if opt.background:
-        for i in l:
-            img=cv2.imread(i)
-            img=cv2.bitwise_and(img,img,mask=mask_img)
-            img=img+back_ground
-            cv2.imwrite(i,img)
 
-    # Remove Background
-    else:
         for i in l:
-            img=cv2.imread(i)
-            cv2.imwrite(i,img)
+
+            img = cv2.imread(i)
+
+            img = cv2.bitwise_and(
+                img,
+                img,
+                mask=mask_img
+            )
+
+            img = img + back_ground
+
+            cv2.imwrite(
+                i,
+                img
+            )
+
+    else:
+
+        for i in l:
+
+            img = cv2.imread(i)
+
+            cv2.imwrite(
+                i,
+                img
+            )
 
     os.chdir("../")
-    cv2.imwrite("./static/finalimg.png", img)
+
+    cv2.imwrite(
+        "./static/finalimg.png",
+        img
+    )
+
+    print("Final image generated successfully")
